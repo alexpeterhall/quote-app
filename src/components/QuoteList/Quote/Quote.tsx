8a895a9f9a0c3b49d66572a37c9b50fba962fd7b
Alex Hall <alexpeterhall@gmail.com>
2023-07-03 06:30:59 -0400

    Reformat quote HTML

--- src/components/QuoteList/Quote/Quote.tsx
+++ src/components/QuoteList/Quote/Quote.tsx
@@ -1,11 +1,26 @@
 type QuoteProps = { quote: Quote }
 
 export default function Quote({ quote }: QuoteProps) {
-  function formatQuote(quote: Quote): string {
+  function formatQuote(quote: Quote) {
     if (quote.tags == null || quote.tags.length === 0) {
-      return `${quote.quote} - ${quote.author}`
+      return (
+        <div>
+          <p data-cy='quote'>{`${quote.quote}`}</p>
+          <p data-cy='author'>{`${quote.author}`}</p>
+        </div>
+      )
     } else {
-      return `${quote.quote} - ${quote.author} - #${quote?.tags.join(' #')}`
+      return (
+        <div>
+          <p data-cy='quote'>{`${quote.quote}`}</p>
+          <p data-cy='author'>{`${quote.author}`}</p>
+          <p data-cy='tagList'>
+            {quote.tags.map((tag) => {
+              return <a key={`${tag}`} href='#' data-cy='tag'>{`#${tag} `}</a>
+            })}
+          </p>
+        </div>
+      )
     }
   }
 
