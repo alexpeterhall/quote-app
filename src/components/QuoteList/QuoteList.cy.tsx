be2cc04f4063a5a6ec760986da4d7b394518791d
Alex Hall <alexpeterhall@gmail.com>
2023-07-04 16:37:05 -0400

    Mount this component with proper context
    Also updates the QA data selectors

--- src/components/QuoteList/QuoteList.cy.tsx
+++ src/components/QuoteList/QuoteList.cy.tsx
@@ -1,16 +1,18 @@
 /// <reference types="Cypress" />
-import QuoteList from './QuoteList'
+import { createApp } from '../../setup'
 
-//TODO Need to bring in Firebase or stub and get data to Quote somehow
 describe('The QuoteList component', () => {
+  beforeEach(() => {
+    cy.mount(createApp())
+  })
+
   it('renders a quote', () => {
-    cy.mount(<QuoteList />)
-    cy.get('[data-cy=quote]').should('have.text', 'This is just a test.')
-    cy.get('[data-cy=author]').should('have.text', 'Testy McTester')
-    cy.get('[data-cy=tagList]').should('not.exist')
+    // cy.mount(createApp())
+    cy.get('[data-qa=quote]').should('be.visible')
+    cy.get('[data-qa=author]').should('be.visible')
+    cy.get('[data-qa=tagList]').should('be.visible')
   })
   it('renders the "Next Quote" button', () => {
-    cy.mount(<QuoteList />)
-    cy.get('[data-cy=nextQuoteButton]').should('have.text', 'Next Quote').and('should.be.clickable')
+    cy.get('[data-qa=NextQuoteButton]').should('have.text', 'Next Quote').and('be.visible')
   })
 })
