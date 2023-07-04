77a428cc93fbfa06d8e439ad9a43654ec73c7351
Alex Hall <alexpeterhall@gmail.com>
2023-07-03 20:12:47 -0400

    Refactor tests for new markup

--- src/components/QuoteList/Quote/Quote.cy.tsx
+++ src/components/QuoteList/Quote/Quote.cy.tsx
@@ -4,14 +4,20 @@ import Quote from './Quote'
 describe('The Quote component', () => {
   it('renders a quote with no tags', () => {
     cy.mount(<Quote quote={{ quote: 'This is just a test.', author: 'Testy McTester', tags: [] }} />)
-    cy.get('[data-cy=quote]').should('have.text', 'This is just a test. - Testy McTester')
+    cy.get('[data-cy=quote]').should('have.text', 'This is just a test.')
+    cy.get('[data-cy=author]').should('have.text', 'Testy McTester')
+    cy.get('[data-cy=tagList]').should('not.exist')
   })
   it('renders a quote with one tag', () => {
     cy.mount(<Quote quote={{ quote: 'This is just a test.', author: 'Testy McTester', tags: ['test'] }} />)
-    cy.get('[data-cy=quote]').should('have.text', 'This is just a test. - Testy McTester - #test')
+    cy.get('[data-cy=quote]').should('have.text', 'This is just a test.')
+    cy.get('[data-cy=author]').should('have.text', 'Testy McTester')
+    cy.get('[data-cy=tagList]').should('have.text', '#test ')
   })
   it('renders a quote with multiple tags', () => {
     cy.mount(<Quote quote={{ quote: 'This is just a test.', author: 'Testy McTester', tags: ['testing', '123'] }} />)
-    cy.get('[data-cy=quote]').should('have.text', 'This is just a test. - Testy McTester - #testing #123')
+    cy.get('[data-cy=quote]').should('have.text', 'This is just a test.')
+    cy.get('[data-cy=author]').should('have.text', 'Testy McTester')
+    cy.get('[data-cy=tagList]').should('have.text', '#testing #123 ')
   })
 })
