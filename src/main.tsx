f64e2e968dd9fb8ab1dddc2ef7ab9e58b096a600
Alex Hall <alexpeterhall@gmail.com>
2023-07-04 07:09:55 -0400

    Remove comments

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// import Firebase, { FirebaseContext } from './services/firebase'
 
-// const MyFirebase = new Firebase()
+const root = createRoot(document.getElementById('root') as Element)
 
-  const root = createRoot(document.getElementById('root') as Element)
-
-  root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 )
