305fbfb08639be49bcc413bb9a4266bcb0fc05a8
Alex Hall <alexpeterhall@gmail.com>
2023-07-03 20:12:26 -0400

    Fix return markup

--- src/components/QuoteList/Quote/Quote.tsx
+++ src/components/QuoteList/Quote/Quote.tsx
@@ -1,28 +1,24 @@
 type QuoteProps = { quote: Quote }
 
 export default function Quote({ quote }: QuoteProps) {
-  function formatQuote(quote: Quote) {
-    if (quote.tags == null || quote.tags.length === 0) {
-      return (
-        <div>
-          <p data-cy='quote'>{`${quote.quote}`}</p>
-          <p data-cy='author'>{`${quote.author}`}</p>
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          <p data-cy='quote'>{`${quote.quote}`}</p>
-          <p data-cy='author'>{`${quote.author}`}</p>
-          <p data-cy='tagList'>
-            {quote.tags.map((tag) => {
-              return <a key={`${tag}`} href='#' data-cy='tag'>{`#${tag} `}</a>
-            })}
-          </p>
-        </div>
-      )
-    }
+  if (quote.tags == null || quote.tags.length === 0) {
+    return (
+      <div>
+        <p data-cy='quote'>{`${quote.quote}`}</p>
+        <p data-cy='author'>{`${quote.author}`}</p>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <p data-cy='quote'>{`${quote.quote}`}</p>
+        <p data-cy='author'>{`${quote.author}`}</p>
+        <p data-cy='tagList'>
+          {quote.tags.map((tag) => {
+            return <a key={`${tag}`} href='#' data-cy='tag'>{`#${tag} `}</a>
+          })}
+        </p>
+      </div>
+    )
   }
-
-  return <div data-cy='quote'>{formatQuote(quote)}</div>
 }
