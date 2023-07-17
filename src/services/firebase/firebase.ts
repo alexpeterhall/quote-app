a7d8b0ed3d34fed8710a3b4a7d122d294031ce04
Alex Hall <alexpeterhall@gmail.com>
2023-07-17 07:22:00 -0400

    Fix config

--- src/services/firebase/firebase.ts
+++ src/services/firebase/firebase.ts
@@ -1,7 +1,22 @@
 import { initializeApp } from 'firebase/app'
 import { get, getDatabase, ref } from 'firebase/database'
 
-const config = { databaseURL: 'https://quotes-20ebd-default-rtdb.firebaseio.com' }
+let config: object
+
+if (window.location.hostname === 'localhost') {
+  config = { databaseURL: import.meta.env.VITE_DATABASE_URL }
+} else {
+  config = {
+    databaseURL: import.meta.env.VITE_DATABASE_URL,
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  }
+}
 
 class Firebase implements FirebaseInstance {
   private app
